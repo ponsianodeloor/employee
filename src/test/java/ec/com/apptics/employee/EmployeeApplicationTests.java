84a0e1a7791b7ee3652ea1738e291ebdb5353aae
Ponsiano De Loor <pdeloor@sudamericano.fin.ec>
2024-04-09 22:40:39 -0500

    Initial commit

---
+++ src/test/java/ec/com/apptics/employee/EmployeeApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.com.apptics.employee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
