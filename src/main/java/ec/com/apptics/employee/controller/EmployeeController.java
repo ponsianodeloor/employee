d1303e61d41cc35eb7f9192002f080edaa7e10d7
Ponsiano De Loor <pdeloor@sudamericano.fin.ec>
2024-04-13 11:19:49 -0500

    obtener empleado por su id

--- src/main/java/ec/com/apptics/employee/controller/EmployeeController.java
+++ src/main/java/ec/com/apptics/employee/controller/EmployeeController.java
@@ -4,9 +4,9 @@ import ec.com.apptics.employee.model.Employee;
 import ec.com.apptics.employee.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -18,6 +18,11 @@ public class EmployeeController {
     @GetMapping("/employee")
     public List<Employee> getEmployee() {
         return employeeService.getEmployees();
+    }
+
+    @GetMapping("/employee/{id}")
+    public Employee getEmployeeById(@PathVariable int id) {
+        return employeeService.getEmployeeById(id);
     }
 
 }
--- src/main/java/ec/com/apptics/employee/service/EmployeeService.java
+++ src/main/java/ec/com/apptics/employee/service/EmployeeService.java
@@ -8,13 +8,18 @@ import java.util.List;
 
 @Service
 public class EmployeeService {
-    List<Employee> employees;
+    List<Employee> employees = new ArrayList<>();
 
-    public List<Employee> getEmployees() {
-        employees = new ArrayList<>();
+    public EmployeeService() {
         employees.add(new Employee(1, "Ponsiano De Loor", "ponsianodeloor@gmail.com"));
         employees.add(new Employee(2, "Thomas Sizalema", "ponsianodeloor@outlook.com"));
+    }
 
+    public List<Employee> getEmployees() {
         return employees;
+    }
+
+    public Employee getEmployeeById(int id) {
+        return employees.stream().filter(e -> e.getId() == id).findFirst().get();
     }
 }
