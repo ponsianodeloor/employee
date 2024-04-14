2a408835b5ee85d0793788978d44bede03dbb40d
Ponsiano De Loor <pdeloor@sudamericano.fin.ec>
2024-04-14 17:22:27 -0500

    instalacion y configuracion de jpa

---
+++ src/main/java/ec/com/apptics/employee/repository/EmployeeRepository.java
@@ -0,0 +1,7 @@
+package ec.com.apptics.employee.repository;
+
+import ec.com.apptics.employee.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+}
