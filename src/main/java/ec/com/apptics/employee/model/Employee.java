a67b5ffcf30161ae3f8d5ecff4edbaa6c48f1361
Ponsiano De Loor <pdeloor@sudamericano.fin.ec>
2024-04-14 18:04:04 -0500

    conexion a base de datos para usar jpa

--- src/main/java/ec/com/apptics/employee/model/Employee.java
+++ src/main/java/ec/com/apptics/employee/model/Employee.java
@@ -1,6 +1,14 @@
 package ec.com.apptics.employee.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
     private String email;
--- src/main/java/ec/com/apptics/employee/service/EmployeeService.java
+++ src/main/java/ec/com/apptics/employee/service/EmployeeService.java
@@ -1,6 +1,8 @@
 package ec.com.apptics.employee.service;
 
 import ec.com.apptics.employee.model.Employee;
+import ec.com.apptics.employee.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -10,35 +12,50 @@ import java.util.List;
 public class EmployeeService {
     List<Employee> employees = new ArrayList<>();
 
+    @Autowired
+    EmployeeRepository employeeRepository;
+
     public EmployeeService() {
         employees.add(new Employee(1, "Ponsiano De Loor", "ponsianodeloor@gmail.com"));
         employees.add(new Employee(2, "Thomas Sizalema", "ponsianodeloor@outlook.com"));
     }
 
     public List<Employee> getEmployees() {
-        return employees;
+        //return employees;
+        return employeeRepository.findAll();
     }
 
     public Employee getEmployeeById(int id) {
-        return employees.stream().filter(e -> e.getId() == id).findFirst().get();
+        //return employees.stream().filter(e -> e.getId() == id).findFirst().get();
+        return employeeRepository.findById(id).orElseThrow(() -> new RuntimeException("Employee not found"));
     }
 
     public Employee addEmployee(Employee employee) {
+        /*
         //get the last employee id
         int id = employees.get(employees.size() - 1).getId();
         employee.setId(id + 1);
         employees.add(employee);
         return employee;
+
+        //employees.add(employee);
+        */
+        return employeeRepository.save(employee);
     }
 
     public Employee updateEmployee(Employee employee, int id) {
+        /*
         Employee emp = employees.stream().filter(e -> e.getId() == id).findFirst().get();
         emp.setName(employee.getName());
         emp.setEmail(employee.getEmail());
         return emp;
+         */
+        employee.setId(id);
+        return employeeRepository.save(employee);
     }
 
     public void deleteEmployee(int id) {
-        employees.removeIf(e -> e.getId() == id);
+        //employees.removeIf(e -> e.getId() == id);
+        employeeRepository.deleteById(id);
     }
 }
